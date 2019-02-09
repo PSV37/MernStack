bb25805be2229e2b8ac8971fa7b83a5fca1b0f87
sapna <sapnapastapure@gmail.com>
2019-02-09 11:44:32 +0530

    added new files

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -2,11 +2,41 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { TextInputField } from "../common/TextInputField";
 import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class AddExperience extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      company: "",
+      title: "",
+      location: "",
+      from: "",
+      to: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
+
   render() {
     return <div />;
   }
 }
 
-export default AddExperience;
+AddExperience.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    profile: state.profile,
+    errors: state.errors
+  };
+};
+
+export default connect(mapStateToProps)(withRouter(AddExperience));
