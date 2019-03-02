eb8aea8f2949a558dc8bbe6936d9fd9d21a15fcb
sapna <sapnapastapure@gmail.com>
2019-03-02 23:24:22 +0530

    new commit

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ app.use(passport.initialize());
 
 require("./config/passport")(passport);
 
-if (process.env.NODE_ENV === "production") {
-  //Set Static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', "index.html"));
-  });
-}
+//if (process.env.NODE_ENV === "production") {
+//Set Static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+});
+//}
 
 // Set Port For Running The Server
 const port = process.env.PORT || 5000;
